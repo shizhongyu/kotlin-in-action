336b4e20725e5ad2320306eeb57ec47b7764d129
lixudong <1966957866@qq.com>
2023-06-12 20:22:55 +0800

    kotlin copy

--- src/ch04/4.3.2.1_DataClassCopy.kt
+++ src/ch04/4.3.2.1_DataClassCopy.kt
@@ -5,4 +5,5 @@ data class Client(val name: String, val postalCode: Int)
 fun main(args: Array<String>) {
     val bob = Client("Bob", 973293)
     println(bob.copy(postalCode = 382555))
+    println(bob)
 }

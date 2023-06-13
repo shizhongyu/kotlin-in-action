976f804707185c3d10bc33f11d6ab132b1dd2329
lixudong <1966957866@qq.com>
2023-06-13 10:42:30 +0800

    伴生对象的作用是在类级别上定义一些方法和属性，而不是实例级别。它们可以像 Java 中的静态方法和属性一样被调用，而不需要创建类的实例。

--- src/ch04/4.4.2_1_CompanionObjects.kt
+++ src/ch04/4.4.2_1_CompanionObjects.kt
@@ -1,6 +1,9 @@
 package ch04.ex4_2_1_CompanionObjects
 
 class A {
+    /**
+     * 伴生对象的作用是在类级别上定义一些方法和属性，而不是实例级别。它们可以像 Java 中的静态方法和属性一样被调用，而不需要创建类的实例。
+     */
     companion object {
         fun bar() {
             println("Companion object called")
